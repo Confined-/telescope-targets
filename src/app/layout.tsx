fcbf10e00ee18b522f0901717c1f5b152682e167
Confined <confined@tuta.io>
2026-09-07 19:44:47 -0700

    Deep red night-vision mode via luminance-to-red matrix filter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col">
+        {/* Red-matrix filter for night-vision mode (see .night-mode). */}
+        <svg width="0" height="0" style={{ position: "absolute" }} aria-hidden="true">
+          <defs>
+            <filter id="night-red" colorInterpolationFilters="sRGB">
+              <feColorMatrix
+                type="matrix"
+                values="0.1807 0.6079 0.0614 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 0"
+              />
+            </filter>
+          </defs>
+        </svg>
         <noscript>
           <div
             style={{
